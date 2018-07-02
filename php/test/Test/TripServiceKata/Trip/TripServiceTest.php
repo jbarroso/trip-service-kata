2e4ace879f1870f462ab808e05b0688ae4be4876
Jose Angel Barroso <jbarroso@antevenio.com>
2018-07-02 18:38:11 +0200

    Testing should return an empty trip list when users are not friends

--- php/test/Test/TripServiceKata/Trip/TripServiceTest.php
+++ php/test/Test/TripServiceKata/Trip/TripServiceTest.php
@@ -4,27 +4,30 @@ namespace Test\TripServiceKata\Trip;
 
 use PHPUnit\Framework\TestCase;
 use TripServiceKata\User\User;
-use TripServiceKata\Trip\TripService;
 use TripServiceKata\Exception\UserNotLoggedInException;
 
 class TripServiceTest extends TestCase
 {
 
-    /**
-     * @var TripService
-     */
-    private $tripService;
-
-    protected function setUp()
-    {
-        $loggedUser = null;
-        $this->tripService = new TestableTripService($loggedUser);
-    }
-
     public function testShouldThrowUserNotLoggedInException()
     {
         $this->expectException(UserNotLoggedInException::class);
+
+        $loggedUser = null;
+        $tripService = new TestableTripService($loggedUser);
+
         $user = new User('myUserName');
-        $this->tripService->getTripsByUser($user);
+        $tripService->getTripsByUser($user);
+    }
+
+    public function testShouldReturnAnEmptyTripListWhenUsersAreNotFriends()
+    {
+        $loggedUser = new User('loggedUserName');
+        $tripService = new TestableTripService($loggedUser);
+
+        $user = new User('myUserName');
+        $trips = $tripService->getTripsByUser($user);
+
+        $this->assertEquals([], $trips);
     }
 }
